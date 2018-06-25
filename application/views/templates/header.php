5882dacdba48bd9126d1e8b034f27e5df3f02d95
asus\user <fadilasetabudi@gmail.com>
2018-06-25 13:02:03 +0700

    Tugas8-10

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -60,6 +60,21 @@
         </div>
       </div>
     </nav>
+    
+     <?php if($this->session->flashdata('user_registered')): ?>
+          <?php echo '<div class="alert alert-success" role="alert">'.$this->session->flashdata('user_registered').'</div>'; ?>
+        <?php endif; ?>
+        <?php if($this->session->flashdata('login_failed')): ?>
+          <?php echo '<div class="alert alert-danger">'.$this->session->flashdata('login_failed').'</div>'; ?>
+        <?php endif; ?>
+
+        <?php if($this->session->flashdata('user_loggedin')): ?>
+          <?php echo '<div class="alert alert-success">'.$this->session->flashdata('user_loggedin').'</div>'; ?>
+        <?php endif; ?>
+
+         <?php if($this->session->flashdata('user_loggedout')): ?>
+          <?php echo '<div class="alert alert-success">'.$this->session->flashdata('user_loggedout').'</div>'; ?>
+        <?php endif; ?>
 
       <header class="masthead text-center text-white d-flex">
       <div class="container my-auto">
